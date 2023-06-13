440734ff221e572f65c90428c69c68ee59862300
Mario Oliver <mariooliver@Marios-MBP.lan>
2023-06-13 12:16:13 -0400

    updated the hero to be responsive to movement and changed gradient into svg

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,18 +3,17 @@ import { FaQuoteLeft, FaQuoteRight, FaHandPeace } from 'react-icons/fa';
 
 const Hero = () => {
   return (
-    <section className="hero" id="home">
+    <section className="hero">
       <div className="hero-banner">
         <h1 className="title">Mario Oliver</h1>
-
-        <p className="text">
-          Hi there <FaHandPeace className="inline-icon"></FaHandPeace> I was
-          destined to create, and I wield the powerful tools of programming,
-          managing blinking boxes, motion graphics, design, and storytelling to
-          bring my visions to life.
-        </p>
         <div className="quote-icons">
           <FaQuoteLeft className="hero-icon"></FaQuoteLeft>
+          <p className="text">
+            Hi there <FaHandPeace className="inline-icon"></FaHandPeace> I was
+            destined to create, and I wield the powerful tools of programming,
+            managing blinking boxes, motion graphics, design, and storytelling
+            to bring my visions to life.
+          </p>
           <FaQuoteRight className="hero-icon"></FaQuoteRight>
         </div>
       </div>
